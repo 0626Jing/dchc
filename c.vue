ea93ad98665cf2fb850f1806b14c4f1703c3e48a
0626Jing <1481436257@qq.com>
2025-05-22 20:52:08 +0800

    c

---
+++ c.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+{{ c }}
+
+
+  </div>
+</template>
+<script setup lang="ts" name="">
+import { ref } from "vue"
+const c = ref(0)
+
+</script>
+<style scoped>
+
+
+
+
+</style>
